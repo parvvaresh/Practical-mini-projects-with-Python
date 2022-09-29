2c2b47c5b76cb3fb3aafc7a7125aecac4687e17b
alireza parvaresh <89921883+parvvaresh@users.noreply.github.com>
2022-09-30 00:59:55 +0330

    Windows 10 Toast Notifications with Python

---
+++ create notif.py
@@ -0,0 +1,7 @@
+
+from win10toast import ToastNotifier
+  
+n = ToastNotifier()
+  
+n.show_toast("Alireza", "Alireza is comming...", duration = 10,
+ icon_path ="https://www.facebook.com/photo/?fbid=184326320629580&set=pob.100071566385123")
